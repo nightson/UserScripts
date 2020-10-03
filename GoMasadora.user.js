9c9f73eaf10d3cf2a94511c00f3df772b8153148
NightsoN Blaze <nightson1988@gmail.com>
2020-10-03 18:08:10 +0800

    Add css style!

--- GoMasadora.user.js
+++ GoMasadora.user.js
@@ -3,17 +3,18 @@
 // @namespace   nightson1988@gmail.com
 // @include     https://www.suruga-ya.jp/product/detail/*
 // @grant       none
-// @version     0.1
+// @version     0.2
 // @author      nightson
 // @description Add a link that redirects the current product page to the corresponding Masadora one.
 // ==/UserScript==
 (function (){
-	var targetNode = document.querySelector(".mgnB10");
+  var targetNode = document.querySelector(".toreka");
 
-	var a = document.createElement("a");
+  var a = document.createElement("a");
 
-	a.text = "移步至Masadora";
-	a.href = document.location.href.replace("https", "http").replace("www.suruga-ya.jp", "surugaya.masadora.jp");
+  a.text = "Go Masadora";
+  a.href = document.location.href.replace("https", "http").replace("www.suruga-ya.jp", "surugaya.masadora.jp");
+  a.style = "background: linear-gradient(#ffa742,#ff711f);display: inline-block;height: 30px;line-height: 30px;font-size: 16px;border: 1px solid #b1b5bd;border-radius: 4px;color: #fff;box-shadow: 0 2px 0 -1px rgba(0,0,0,.1);position: relative;cursor: pointer;width: 138px;text-align: center;font-family: Arial;";
 
-	targetNode.parentNode.insertBefore(a, targetNode);
+  targetNode.parentNode.insertBefore(a, targetNode);
 })();
