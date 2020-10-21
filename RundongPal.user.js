cad65435ae9cff72a8055f6703228249faf58a1a
NightsoN Blaze <nightson1988@gmail.com>
2020-10-21 11:57:16 +0800

    Create RundongPal.user.js

---
+++ RundongPal.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Rundong Pal
+// @namespace   nightson1988@gmail.com
+// @match       http://rundongex.com/Forms/Member/WayBill/WayBill_Add.aspx
+// @grant       none
+// @version     0.1
+// @author      nightson
+// @description Script that makes Rundong more user-friendly.
+// ==/UserScript==
+(function (){
+  let nodes = document.querySelectorAll('#collapseContent1 td[id^="td_remark_"] a');
+
+  nodes.forEach(function (node){
+    node.innerHTML = node.querySelector('span').getAttribute('title2');
+    node.removeAttribute('href');
+    node.removeAttribute('one-link-mark');
+  });
+})();
