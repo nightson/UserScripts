b3d5a51876173b2634f28ea72c6f6825a0c914b3
NightsoN Blaze <nightson1988@gmail.com>
2020-10-22 02:05:30 +0000

    Create GoZenmarket.user.js

---
+++ GoZenmarket.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        ZenMarket, Please
+// @namespace   nightson1988@gmail.com
+// @include     https://page.auctions.yahoo.co.jp*auction/*
+// @grant       none
+// @version     0.1
+// @author      nightson
+// @description Add a link that redirects the current product page to the Zenmarket one.
+// @icon        https://zenmarket.jp/favicon.ico?1
+// ==/UserScript==
+(function (){
+  var targetNode = document.querySelector(".Price__buttonArea a");
+
+  var a = document.createElement("a");
+      br = document.createElement("br");
+
+  a.text = "Go ZenMarket";
+  a.href = "https://zenmarket.jp/auction.aspx?itemCode=" + document.location.href.match(/[\d,\w]{9,10}/g);
+  a.setAttribute("class", "Button--buynow");
+
+  targetNode.parentNode.insertBefore(a, targetNode);
+  targetNode.parentNode.insertBefore(br, targetNode);
+})();
