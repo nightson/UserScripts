4201e00eec06be73f167d6dd2539d8b447b461cc
NightsoN Blaze <nightson1988@gmail.com>
2020-10-31 10:05:31 +0000

    Update MasadoraPlease.user.js

--- MasadoraPlease.user.js
+++ MasadoraPlease.user.js
@@ -15,7 +15,7 @@
   var a = document.createElement("a");
 
   a.text = "Go Masadora";
-  a.href = document.location.href.replace("https", "http").replace("www.suruga-ya.jp", "surugaya.masadora.jp");
+  a.href = document.location.href.replace("www.suruga-ya.jp", "surugaya.masadora.jp");
   a.style = "background: linear-gradient(#ffa742,#ff711f);display: inline-block;height: 30px;line-height: 30px;font-size: 16px;border: 1px solid #b1b5bd;border-radius: 4px;color: #fff;box-shadow: 0 2px 0 -1px rgba(0,0,0,.1);position: relative;cursor: pointer;width: 138px;text-align: center;font-family: Arial;";
 
   targetNode.parentNode.insertBefore(a, targetNode);
